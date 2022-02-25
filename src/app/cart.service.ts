5050621b47f0ebe5781f8795e8e89ae4a997683e
JACaballeroM <jcaballero@bidtecnologia.mx>
2022-02-25 13:23:17 -0600

    ShoppingCart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Product } from '../products';
+import { Product } from './products';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,7 @@ export class CartService {
   }
 
   getItems(){
-    return this.itmes;
+    return this.items;
   }
 
   clearCart(){
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -13,13 +13,18 @@ export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
   constructor(
     private route: ActivatedRoute,
-    private service: CartService
+    private cartService: CartService
     ) { }
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
     this.product = products.find(product=>product.id===productIdFromRoute) ;
+  }
+
+  addToCart(product:Product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart.');
   }
 
 }
