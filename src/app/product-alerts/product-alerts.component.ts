12cfad2def402ed3391f2013b0ea67b3d567fdde
JACaballeroM <jcaballero@bidtecnologia.mx>
2022-02-25 12:58:55 -0600

    ProductDetails

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,9 +7,8 @@ import {Product} from '../products';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent {
-
+ 
 @Input() product: Product|undefined;
 @Output() notify = new EventEmitter();
-  
 
 }
